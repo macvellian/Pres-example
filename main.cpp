8e462463502e369c074d1f0fac5bbf2cbdc78440
Max <mamackay@uvm.edu>
2025-02-12 19:04:55 -0500

    Edited main.cpp file from branch3

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello from the new branch!" << std::endl;
     std::cout << "Hello from the branch3!" << std::endl;
+    std::cout << "Hello from the new branch!" << std::endl;
     return 0;
 }
